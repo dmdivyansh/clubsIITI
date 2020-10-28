a2be2b41f0ec3f0dba700b0f7ebdb97a0a29ce33
= <garvitgalgat@gmail.com>
2020-10-28 15:46:20 +0530

    OAuth added

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session, url_for
 from flask_mysqldb import MySQL
 import MySQLdb
 import yaml
+from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
 
-# COnfigure db
+# Configure db
 db = yaml.load(open('db.yaml'), Loader=yaml.FullLoader)
 
 app.config['MYSQL_HOST'] = db['mysql_host']
@@ -15,10 +16,55 @@ app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
 
+# OAuth Config
+app.secret_key = db['secret_key']
+oauth = OAuth(app)
+google = oauth.register(
+    name="google",
+    client_id=db['client_id'],
+    client_secret=db['client_secret'],
+    access_token_url="https://accounts.google.com/o/oauth2/token",
+    access_token_params=None,
+    authorize_url="https://accounts.google.com/o/oauth2/auth",
+    authorize_params=None,
+    api_base_url="https://www.googleapis.com/oauth2/v1/",
+    userinfo_endpoint=
+    "https://openidconnect.googleapis.com/v1/userinfo",  # This is only needed if using openId to fetch user info
+    client_kwargs={"scope": "openid email profile"},
+)
+
 
 @app.route("/")
 def cultural():
+    email = dict(session).get("email", None)
+    print("current user:", email)
     return render_template('home.html')
+
+
+@app.route("/login")
+def login():
+    google = oauth.create_client("google")
+    redirect_uri = url_for("authorize", _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@app.route("/authorize")
+def authorize():
+    google = oauth.create_client("google")
+    token = google.authorize_access_token()
+    resp = google.get("userinfo", token=token)
+    user_info = resp.json()
+    # do something with the token and profile
+    session["email"] = user_info["email"]
+    print(user_info)
+    return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect("/")
 
 
 @app.route("/clubs/<clubName>")
