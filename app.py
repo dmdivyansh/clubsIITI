81c3a8aec9847d3239353c677e771c8deb269131
Apurvam18 <1047divyansh@gmail.com>
2020-11-12 22:48:59 +0530

    used images yaml

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 mysql = MySQL(app)
+
+img= yaml.load(open('images.yaml'), Loader=yaml.FullLoader)
+print('++++++++++++++++++++++++')
+print(img)
+
 # --------------------------------------
 
 
@@ -103,7 +108,8 @@ def index():
     
     name = dict(session).get("name", None)
     print("current user:", name)
-    return render_template('home.html', name=name, msg=msg, msg_alert=msg_alert)
+
+    return render_template('home.html', name=name, msg=msg, msg_alert=msg_alert,img=img)
 
 
 
@@ -113,7 +119,7 @@ def myDetails():
     if(email == None):
         msg = "Please signin into CLUBSIITI"
         msg_alert = "warning"
-        return render_template('home.html', msg=msg, msg_alert=msg_alert)
+        return render_template('home.html', msg=msg, msg_alert=msg_alert,img=img)
     
     else:
         cur = mysql.connection.cursor()
@@ -178,7 +184,9 @@ def club(clubName):
     member = False
     verified = False
     notexist = True
-    imageUrl = clubName + ".jpg"
+    imageUrl = img[clubName]
+    print('-----------------')
+    print(imageUrl)
     # -----------------------------------------
 
     # verifying the current email id ---------------
